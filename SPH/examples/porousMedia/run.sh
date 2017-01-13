1c9ffc3ee9e819b4c41df7845a61a757fba92aab
YJ Mao <ymao.mu@gmail.com>
2017-01-13 09:54:06 -0600

    correct path to exe

--- SPH/examples/porousMedia/run.sh
+++ SPH/examples/porousMedia/run.sh
@@ -3,8 +3,8 @@
 set -e
 set -u
 
-lmp=../../../src/lmp_mpi
+lmp=../../src/lmp_mpi
 np=4
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-mpirun -np ${np} ${lmp} -in  in.rubberRings3
+mpirun -np ${np} ${lmp} -in  in.porous
